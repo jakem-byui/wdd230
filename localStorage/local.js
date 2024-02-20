1b3cf323fb3e590b4b2e44bd42581f2f17e2466c
Jake Matheson <131726132+jakem-byui@users.noreply.github.com>
2024-02-19 22:29:45 -0800

    local storage video

---
+++ localStorage/local.js
@@ -0,0 +1,9 @@
+// Storing data
+localStorage.setItem('username', 'JohnDoe');
+
+// Retrieving data
+// Outputs: JohnDoe
+console.log(localStorage.getItem('username'));
+
+// Remove data
+// localStorage.removeItem('username');
