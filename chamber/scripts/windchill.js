1196291e3c89ca35aea3d0c9e75de962052fb831
Jake Matheson <131726132+jakem-byui@users.noreply.github.com>
2024-02-24 14:25:42 -0800

    I think i fixed the weweather

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -6,10 +6,12 @@ temperature = parseInt(temperature_element.textContent);
 wind_speed = parseInt(wind_speed_element.textContent);
 wind_chill = 35.74 + (.6215 * temperature) - (35.75 * (wind_speed ** .16)) + (.4275 * temperature * (wind_speed ** .16));
 
-if (wind_speed > 3) {
+if (wind_speed < 3) {
     wind_chill_element.textContent = "N/A";
-} else if (temperature <= 50) {
+}
+else if (temperature >= 50) {
     wind_chill_element.textContent = "N/A";
-} else {
+}
+else {
     wind_chill_element.textContent = wind_chill.toFixed(2);
 }
