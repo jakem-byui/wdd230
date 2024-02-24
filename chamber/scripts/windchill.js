5f8db8d6a8ad65cb019b0d28708ec297eb946af8
Jake Matheson <131726132+jakem-byui@users.noreply.github.com>
2024-02-24 14:13:22 -0800

    windchill and picture updates

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,15 @@
+temperature_element = document.querySelector("#temperature");
+wind_speed_element = document.querySelector("#wind-speed");
+wind_chill_element = document.querySelector("#wind-chill");
+
+temperature = parseInt(temperature_element.textContent);
+wind_speed = parseInt(wind_speed_element.textContent);
+wind_chill = 35.74 + (.6215 * temperature) - (35.75 * (wind_speed ** .16)) + (.4275 * temperature * (wind_speed ** .16));
+
+if (wind_speed_element => 3) {
+    wind_chill_element.textContent = "N/A";
+} else if (temperature_element => 50) {
+    wind_chill_element.textContent = "N/A";
+} else {
+    wind_chill_element.textContent = wind_chill.toFixed(2);
+}
