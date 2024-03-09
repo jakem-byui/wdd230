aae82a027aa594971f5631d306be1146709d82d9
Jake Matheson <131726132+jakem-byui@users.noreply.github.com>
2024-03-09 11:04:43 -0800

    json practice

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,8 +1,7 @@
-const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
-
 const cards = document.querySelector('#cards');
 
-async function getProphetData(url) {
+async function getProphetData() {
+    const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
     const response = await fetch(url);
     const data = await response.json();
     //console.table(data.prophets);
@@ -17,10 +16,19 @@ const displayProphets = (prophets) => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
         let fullName = document.createElement('h2'); // Use 'h2' for the full name
+        let birthdate = document.createElement('p');
+        let birthPlace = document.createElement('p');
         let portrait = document.createElement('img');
 
         // Build the h2 content out to show the prophet's full name
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        // Birthdate
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+
+        // Birth Place
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
         // Build the image portrait by setting all the relevant attributes
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
@@ -30,6 +38,8 @@ const displayProphets = (prophets) => {
 
         // Append the section(card) with the created elements
         card.appendChild(fullName);
+        card.appendChild(birthdate);
+        card.appendChild(birthPlace);
         card.appendChild(portrait);
 
         cards.appendChild(card);
